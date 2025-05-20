bc3767510763ed2c4c364322f50acf75056716c9
norangsuji <sujihong93@gmail.com>
2025-05-20 17:56:04 +0900

    🗑️ chore: 빌드 확인

---
+++ SELabPage/src/Pages/MainPage.jsx
@@ -0,0 +1,13 @@
+// MainPage.jsx
+import { Routes, Route } from "react-router-dom";
+
+function MainPage() {
+  return (
+    <div>
+      <h1>Main Page</h1>
+      <p>빌드 상태 확인용 예시 페이지입니다 💡</p>
+    </div>
+  );
+}
+
+export default MainPage;
